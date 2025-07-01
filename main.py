9e8a35246b5f58c7c01c7c2560f44a907805ad34
Giovanna Esteves <gio_esteves@hotmail.com>
2025-07-01 20:55:32 -0300

    atualizando o projeto para os parametros do professor

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import time
 from memoria_virtual.GerenciadorMemoriaVirtual import GerenciadorMemoriaVirtual
 
 def simular_processo(gerenciador, thread_id, acessos):
-    for pagina in acessos:
-        gerenciador.acessar_endereco(thread_id, pagina)
-        time.sleep(0.5)  # Simula tempo de acesso/processamento
+    for pagina, operacao in acessos:
+        gerenciador.acessar_endereco(thread_id, pagina, operacao)
+        time.sleep(0.5)  # Simula tempo de processamento
 
 if __name__ == "__main__":
     gerenciador = GerenciadorMemoriaVirtual()
 
     # Acessos simulados por cada processo de endereços de paginas virtuais
-    acessos_thread_1 = [0, 1, 2, 3, 0, 4, 5]
-    acessos_thread_2 = [2, 3, 4, 6, 2, 7]
+    acessos_thread_1 = [(0, "R"), (1, "W"), (2, "R"), (3, "W")] # lista de tuplas
+    acessos_thread_2 = [(2, "R"), (4, "W"), (6, "R"), (7, "W")]
 
     # Threads simultâneas acessando memória
     thread1 = threading.Thread(target=simular_processo, args=(gerenciador, 1, acessos_thread_1))
